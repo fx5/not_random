3287b1f5c965672ef7f1a97ab00b16b5e64a5f0a
unknown <egillier@gmail.com>
2019-05-13 14:21:55 +0200

    Add output bits length argument between 1 and 8

--- rebuild_random.py
+++ rebuild_random.py
@@ -7,6 +7,8 @@ from itertools import izip
 from progress import ProgressBar
 import cPickle as pickle
 from util import N
+import sys
+import os
 
 
 def rebuild_random(magic, data):
@@ -29,7 +31,7 @@ def rebuild_random(magic, data):
         xor_data = reduce(lambda a, b: a ^ b, xor_data, 0)
         # XOR the bits of the result-byte
         xor_data = reduce(lambda a, b: a ^ b,
-                          (xor_data >> i for i in xrange(8)))
+                          (xor_data >> i for i in xrange(output_bits_length)))
         xor_data &= 1
         state[bit_pos // 32] |= xor_data << (31 - bit_pos % 32)
 
@@ -47,14 +49,32 @@ def random_string(length, random_obj=random):
     :param random.Random random_obj: random object (default: random module)
     :return: str
     """
-    return "".join(chr(random_obj.randint(0, 255)) for _ in xrange(length))
+    return "".join(chr(random_obj.randint(0, (2**output_bits_length) - 1)) for _ in xrange(length))
 
 
 def main():
     """Main function"""
 
+    global output_bits_length
+
+    # 1 optional argument : output bits length
+    if len(sys.argv) > 1:
+        output_bits_length = int(sys.argv[1])
+        if output_bits_length > 8:
+            print "argument %d bits output invalid, currently supported : integer between 1 and 8 inclusive" % output_bits_length
+            return
+        print "Using argument %d bits output" % output_bits_length
+    else:
+        output_bits_length = 8
+        print "Using default 8 bits output"
+
+    magic_filename = "magic_data_"+str(output_bits_length)
+    if not os.path.isfile(magic_filename):
+        print "Error: you need to first generate Magic data for %d bits output using: \npython ./gen_magic_data.py %d" % (output_bits_length, output_bits_length)
+        return
+
     print "Loading Magic"
-    with closing(gzip.GzipFile("magic_data")) as f:
+    with closing(gzip.GzipFile(magic_filename)) as f:
         magic = pickle.load(f)
     print "Done."
 
